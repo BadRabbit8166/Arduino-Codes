b5845b0f203d282d012f3b6fef1d27a574c152b9
Daito Shimatsu <33637021+Robotakademie@users.noreply.github.com>
2018-11-14 21:25:01 +0100

    Added updated codes and one new Project

---
+++ Spendenbox/Spendenbox.ino
@@ -0,0 +1,63 @@
+/*Projekt Spendenbox
+ * (C) Felix Beer
+ * 10.11.2018
+ */
+
+#include <Stepper.h> //Schrittmotor-Bibliothek laden
+#include <Servo.h> //Servo-Bibliothek laden
+
+int SPU = 2048; //Schritte pro Umdrehung definieren
+int rgbrot = 12;
+int rgbgruen = 11;
+int rgbblau = 10;
+int taster = 9;
+int reset = 2;
+int tasterstatus = 0;
+
+//Die Motoren definieren
+Stepper katze(SPU, 3, 5, 4, 6); //Der Schrittmotor heisst Katze
+Servo klappe; //Der Servo heisst jetzt klappe
+
+//Alles einstellen
+void setup()
+{
+  katze.setSpeed(5);
+  klappe.attach(8);
+  pinMode(taster, INPUT);
+  pinMode(rgbrot, OUTPUT);//RGB-LED einstellen
+  pinMode(rgbgruen, OUTPUT);
+  pinMode(rgbblau, OUTPUT);
+}
+  
+
+void loop(){
+  digitalWrite(rgbgruen,HIGH);
+  tasterstatus = digitalRead(taster);
+  if(tasterstatus == HIGH){
+    digitalWrite(rgbgruen,LOW);
+    digitalWrite(rgbblau,HIGH);
+    klappe.write(130);
+    delay(3000);
+    klappe.write(100);
+    delay(1000);
+    delay(1000);
+    digitalWrite(rgbgruen,HIGH);
+    katze.step(-500);
+    delay(5000);
+    katze.step(500);
+    delay(1000);
+    digitalWrite(rgbblau,LOW);
+    digitalWrite(rgbgruen,LOW);
+    delay(1000);
+    digitalWrite(rgbrot, HIGH);
+    delay(1000);
+    digitalWrite(rgbrot, LOW);
+    delay(1000);
+    digitalWrite(rgbrot, HIGH);
+    delay(1000);
+    digitalWrite(rgbrot, LOW);
+    digitalWrite(reset, HIGH);
+    delay(1000);
+  }
+}
+
---
+++ WAND_anhalten/WAND_anhalten.ino
@@ -0,0 +1,43 @@
+int schneller = 100;
+int langsam = 80;
+int mr = 5;
+int ml = 6;
+int trigger=7; 
+int echo=12; 
+long dauer=0; 
+long entfernung=0; 
+void setup()
+{
+Serial.begin (9600); 
+pinMode(trigger, OUTPUT); 
+pinMode(echo, INPUT); 
+pinMode(mr, OUTPUT);
+pinMode(ml,OUTPUT);
+}
+void loop()
+{
+digitalWrite(trigger, LOW); 
+delay(5); 
+digitalWrite(trigger, HIGH); 
+delay(10);
+digitalWrite(trigger, LOW);
+dauer = pulseIn(echo, HIGH); 
+entfernung = (dauer/2) * 0.03432; 
+if (entfernung <= 50) 
+{
+digitalWrite(mr, LOW);
+digitalWrite(ml, LOW);
+delay(2000);
+analogWrite(ml, langsam);
+delay(1800);
+digitalWrite(ml, LOW);
+}
+else 
+{
+Serial.print(entfernung); 
+Serial.println(" cm"); 
+analogWrite(mr, schneller);
+analogWrite(ml, schneller);
+}
+delay(1000); 
+}
